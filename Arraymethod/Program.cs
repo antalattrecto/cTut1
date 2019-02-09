15f82ba9910a2cdd7a4a61bba929561923f2c3d1
Antal Nagy <antal.nagy@invenias.com>
2019-02-09 20:50:14 +0000

    arraymethod commit

---
+++ Arraymethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraymethod
+{
+    class AMethod
+    {
+        private int[] myArray = { 1, 2, 3, 4, 5 };
+        public int[] MyArray
+        {
+            get
+            {
+                return myArray;
+            }
+            set
+            {
+
+            }
+        }
+        
+        public void PrintFirstElement(int[] a)
+        {
+            Console.WriteLine("First element: {0}", a[0]);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AMethod GetFirst = new AMethod();
+
+            GetFirst.PrintFirstElement(GetFirst.MyArray);
+            Console.ReadLine();
+        }
+    }
+}
