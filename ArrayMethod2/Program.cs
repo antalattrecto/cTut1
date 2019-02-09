b8477b4f96daa5ed63109ac4189501e4c6df59a9
Antal Nagy <antal.nagy@invenias.com>
2019-02-09 22:52:53 +0000

    arraymethod2 commit

---
+++ ArrayMethod2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMethod
+{
+    class ListA
+    {
+        private List<int> myList = new List<int> { 1, 2, 3, 4, 5 };
+
+        public List<int> MyList
+        {
+            get
+            {
+                return myList;
+            }
+            set
+            {
+
+            }
+        }
+
+        public void PrintFirstListElement (List<int> a)
+        {
+            Console.WriteLine("First element is: {0}", a[0]);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListA GetStuff = new ListA();
+
+            GetStuff.PrintFirstListElement(GetStuff.MyList);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ListMethod2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMethod2
+{
+    class UserInput
+    {
+        private int[] a = new int[5];
+        private List<int> l = new List<int>();
+
+        public int[] A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+
+            }
+        }
+
+        public List<int> L
+        {
+            get
+            {
+                return l;
+            }
+            set
+            {
+
+            }
+        }
+
+        public List<int> GatherInputL()
+        {
+            int input;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter integer:");
+                input = Convert.ToInt32(Console.ReadLine());
+                L.Add(input);
+            }
+            return L;
+        }
+
+        public List<int> ReturnInputL()
+        {
+            List<int> resultList = GatherInputL();
+
+            for(int i=0; i<1;i++)
+            {
+                resultList.ForEach(Console.Write);
+                
+            }
+            return resultList;
+        }
+
+        public int[] GatherInput()
+        {
+            for (int i = 0; i<A.Length; i++)
+            {
+                Console.Write("Input your int:");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine("Int added to the array");
+            }
+            return A;
+        }
+
+        public void ReturnInput()
+        {
+            for (int i =0; i<A.Length; i++)
+            {
+                Console.Write("{0}, ", A[i]);
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UserInput GetStuff = new UserInput();
+
+            GetStuff.GatherInput();
+            GetStuff.ReturnInput();
+            Console.ReadLine();
+            GetStuff.GatherInputL();
+            GetStuff.ReturnInputL();
+            Console.ReadLine();
+
+        }
+    }
+}
