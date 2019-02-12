cc04314021288d0fc03b3d5cbf0609be585c0aa9
Antal Nagy <antal.nagy@invenias.com>
2019-02-12 21:44:22 +0000

    fourth commit

---
+++ AnotherInherit/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace AnotherInherit
+{
+    class BaseClass
+    {
+       private static DateTime currentDate = DateTime.Now.Date;
+       public int currentYear = currentDate.Year;
+       public int userAge;
+       private int userYearOfBirth;
+
+        public int UserYearOfBirth
+        {
+            get
+            {
+                return userYearOfBirth;
+            }
+            set
+            {
+                if (value < 1900 || value > 3000)
+                    Console.WriteLine("Type in a year between 1900 and 3000");
+                else
+                    userYearOfBirth = value;
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            return "\nCurrent year is: " + currentYear + "\nUser's year of birth: " + UserYearOfBirth + "\nUser's age is: " + userAge;
+        }
+
+       
+    }
+
+    class ChildClass: BaseClass
+    {
+        public void GetBirth()
+        {
+            Console.Write("Type in your year of birth: ");
+            UserYearOfBirth = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public int CalculateAge()
+        {
+
+            userAge = currentYear - UserYearOfBirth;
+
+            return userAge;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ChildClass obj = new ChildClass();
+            
+
+            obj.GetBirth();
+            obj.CalculateAge();
+
+          Console.WriteLine(obj.ToString());
+  
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Inherit1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit1
+{
+    class BaseClass
+    {
+        private static DateTime currentDate = DateTime.Now.Date;
+        public int currentYear = currentDate.Year;
+     
+
+        public override string ToString()
+        {
+            return "\nCurrent Year is: " + currentYear + "\nUser's date fo birth is: " + userDateOfBirth + "\nUser's Age is: " + userAge;
+        }
+
+        public int userAge { get; set; }
+        public int userDateOfBirth { get; set; }
+
+        public int CalculateAge(int currentYear, int userDateOfBirth)
+
+
+        {
+            userAge = currentYear - userDateOfBirth;
+
+            return userAge;
+        }
+
+       
+    }
+
+    class ChildClass:BaseClass
+    {
+        public void UserInput()
+        {
+
+            CalculateAge(currentYear, 34);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass obj = new ChildClass();
+
+            obj.UserInput();
+
+            Console.ReadLine();
+
+        }
+    }
+}
