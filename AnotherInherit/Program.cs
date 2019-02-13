06b20a3671e15aee69aa67d1be68915403f7888b
Antal Nagy <antal.nagy@invenias.com>
2019-02-13 20:06:44 +0000

    fix path

--- AnotherInherit/Program.cs
+++ AnotherInherit/Program.cs
@@ -19,9 +19,14 @@ namespace AnotherInherit
             set
             {
                 if (value < 1900 || value > 3000)
+                {
                     Console.WriteLine("Type in a year between 1900 and 3000");
+                  
+                }
                 else
+                {
                     userYearOfBirth = value;
+                }
             }
         }
 
@@ -30,6 +35,14 @@ namespace AnotherInherit
         public override string ToString()
         {
             return "\nCurrent year is: " + currentYear + "\nUser's year of birth: " + UserYearOfBirth + "\nUser's age is: " + userAge;
+        }
+
+
+        public virtual void Exit()
+        {
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadLine();
+
         }
 
        
@@ -51,6 +64,12 @@ namespace AnotherInherit
             return userAge;
         }
 
+        public override void Exit()
+        {
+            Console.WriteLine("Thank you and Bye now!");
+            System.Threading.Thread.Sleep(2000);
+        }
+
     }
     class Program
     {
@@ -58,14 +77,26 @@ namespace AnotherInherit
         {
             
             ChildClass obj = new ChildClass();
+            BaseClass obj1 = new BaseClass();
+
             
 
             obj.GetBirth();
             obj.CalculateAge();
 
-          Console.WriteLine(obj.ToString());
+            if (obj.userAge == obj.currentYear)
+            {
+                obj1.Exit();
+            }
+
+            else
+            {
+                Console.WriteLine(obj.ToString());
+                obj.Exit();
+            }
+
   
-            Console.ReadLine();
+            
         }
     }
 }
