d0821eec114d1df2855930889499e52fc696b9a6
Antal Nagy <antal.nagy@invenias.com>
2019-02-25 13:17:34 +0000

    more constructor examples

---
+++ Constructor2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructor2
+{
+    public class Employee
+    {
+        public int Salary;
+
+        public Employee(int annualSalary)
+        {
+            Salary = annualSalary;
+        }
+
+        public Employee(int weeklySalary, int numberOfWeeks)
+        {
+            Salary = weeklySalary * numberOfWeeks;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public Manager(int annualSalary) : base(annualSalary) { }
+        public Manager(int weeklySalary, int numberOfWeeks) : base(weeklySalary, numberOfWeeks) { }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager obj1 = new Manager(45000);
+            Manager obj2 = new Manager(866, 52);
+
+            Console.WriteLine("{0}, {1}", obj1.Salary, obj2.Salary);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorPrivate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConstructorPrivate
+{
+    public class Counter
+    {
+        private Counter() { }
+        public static int currentCount;
+        public static int IncrementCount()
+        {
+            return ++currentCount;
+        }
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            //Counter aCounter = new Counter();
+
+            Counter.currentCount = 0;
+            Counter.IncrementCount();
+            Console.WriteLine("New count: {0}", Counter.currentCount);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
