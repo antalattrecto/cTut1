be76178b6dd0397dadb24ef6295a8aa2cc398f13
Antal Nagy <antal.nagy@invenias.com>
2019-02-25 16:18:31 +0000

    remove whitespace and unused ref from list

--- Lists3/Program.cs
+++ Lists3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Lists3
@@ -93,27 +91,6 @@ namespace Lists3
                 Console.WriteLine(customer);
             }
             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             Console.ReadLine();
         }
     }
